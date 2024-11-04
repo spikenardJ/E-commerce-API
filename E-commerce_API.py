85c0699150fd8bbb2da42019ac540ac299cba460
spikenardJ <168881170+spikenardJ@users.noreply.github.com>
2024-11-04 11:28:43 -0600

    Update E-commerce_API.py

--- E-commerce_API.py
+++ E-commerce_API.py
@@ -9,7 +9,7 @@ from marshmallow import fields
 from marshmallow import ValidationError
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:!Jaedyn77@localhost/e_commerce_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:"*********"@localhost/e_commerce_db"
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
